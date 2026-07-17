279c057c501992491ea560ebc55d7e9a18a30664
HeitorrFerrari <heitorferrari9@gmail.com>
2026-07-17 16:04:02 -0300

    test(db_tests): atualizar testes para extrair e salvar curriculo no banco de dados

--- app/tests/db_tests/test_resume_repository.py
+++ app/tests/db_tests/test_resume_repository.py
@@ -2,20 +2,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from app.db.repositories.job_repository import get_job, save_job
-from app.schemas.job_requirements import JobRequirements
+from app.api.extraction.extract_data import extrair_curriculo
+from app.db.repositories.resume_repository import get_resume, save_resume
+from app.extractions.pdf.pdf_extractor import extrair_texto_pdf
+from app.guard.guard import check_document_format, check_extracted_text
 
 if __name__ == "__main__":
-    job = JobRequirements(
-        title="Desenvolvedor Backend Pleno",
-        required_skills=["Python", "SQL", "Git"],
-        desired_skills=["Docker", "FastAPI"],
-        min_years_experience=2,
-        min_education="Graduação em Ciência da Computação ou correlatas",
-    )
+    caminho = "docs/Curriculo-3.pdf"
+    check_document_format(caminho)
+    texto = extrair_texto_pdf(caminho)
+    check_extracted_text(texto)
+    resume = extrair_curriculo(texto)
 
-    job_id = save_job(job)
-    print("Salvo com id: ", job_id)
+    resume_id = save_resume(resume)
+    print("salvo com id:", resume_id)
 
-    job_recuperado = get_job(job_id)
-    print(job_recuperado.model_dump_json(indent=2))
+    resume_recuperado = get_resume(resume_id)
+    print(resume_recuperado.model_dump_json(indent=2))
