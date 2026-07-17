6bb06defd103fa6ebe8ea479300e7aa33bd45ad0
HeitorrFerrari <heitorferrari9@gmail.com>
2026-07-17 15:23:17 -0300

    feat(db): adicionar funções para salvar e buscar resultados de validação e incluir testes unitários

--- app/db/repositories/validation_repository.py
+++ app/db/repositories/validation_repository.py
@@ -4,3 +4,14 @@ from app.db.mongo import db
 from app.schemas.validation_result import ValidationResult
 
 collection = db["validation"]
+
+def save_validation_result(validation_result: ValidationResult) -> str:
+    validation = collection.insert_one(validation_result.model_dump())
+    return str(validation.inserted_id)
+
+def get_validation_result(validation_id: str) -> ValidationResult:
+    documento = collection.find_one({"_id": ObjectId(validation_id)})
+    if documento is None:
+        return None
+    documento.pop("_id")
+    return ValidationResult(**documento)
