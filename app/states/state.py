b74c1e214c76ef305b107a9d2346023700efe022
HeitorrFerrari <heitorferrari9@gmail.com>
2026-07-23 10:14:43 -0300

    feat(app): adicionar estado TypedDict para validação e criar arquivo validator.py vazio

---
+++ app/states/state.py
@@ -0,0 +1,12 @@
+from typing import Optional, TypedDict
+
+from app.schemas.extracted_resume import CurriculoExtraido
+from app.schemas.validation_result import ValidationResult
+from app.schemas.job_requirements import JobRequirements
+
+class GraphState(TypedDict):
+    resume_text: str
+    job_requirements: JobRequirements
+    resume: Optional[CurriculoExtraido]
+    validation: Optional[ValidationResult]
+    feedback: Optional[str]
