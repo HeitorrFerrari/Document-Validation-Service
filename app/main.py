5bf7060b87f8c9eab7cc9845829fd216b68ecdc6
HeitorrFerrari <heitorferrari9@gmail.com>
2026-09-24 17:47:24 -0300

    fix(app): corrigir cabeçalho de expiração em resposta HTTP para desabilitar cache estático

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ async def disable_static_cache(request, call_next):
         response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
         response.headers["Pragma"] = "no-cache"
         response.headers["Expires"] = "0"
+
     return response
 
 
